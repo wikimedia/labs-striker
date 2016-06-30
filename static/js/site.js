2bb7e206dc9e7299fc940b2657d961c2237c260d
Bryan Davis <bd808@wikimedia.org>
2016-06-30 17:30:16 -0600

    Use more descriptive naming for account settings
    Use more descriptive section labels and add tooltips for the LDAP and
    SUL account settings.

---
+++ static/js/site.js
@@ -0,0 +1,8 @@
+/*!
+ * Copyright (c) 2016 Wikimedia Foundation and contributors.
+ * Licensed under the GPL v3+ license
+ */
+$(function () {
+    "use strict";
+    $('[data-toggle="tooltip"]').tooltip();
+})
--- striker/labsauth/views.py
+++ striker/labsauth/views.py
@@ -108,6 +108,6 @@ def oauth_callback(req):
     req.user.realname = sul_user['realname']
     req.user.save()
 
-    messages.info(req, _(
-        "Authenticated as SUL user {user}".format(user=sul_user['username'])))
+    messages.info(req, _("Authenticated as OAuth user {user}".format(
+            user=sul_user['username'])))
     return shortcuts.redirect(req.session.get(NEXT_PAGE, '/'))
