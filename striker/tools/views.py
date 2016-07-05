wikimedia/labs-striker

commit 33520dc95de9054359fba0c011b0f198a8430a1d

Rearrange URLs

striker/tools/views.py
@@ -140,24 +140,24 @@ def repo_create(req, tool):
 
 
 @inject_tool
-def repo_view(req, tool, name):
+def repo_view(req, tool, repo):
     ctx = {
         'tool': tool,
-        'repo_name': name,
+        'repo': repo,
         'status': 'unknown',
         'urls': [],
         'policy': {'view': None, 'edit': None, 'push': None},
     }
     try:
-        repo = phab.get_repository(name)
-        ctx['status'] = repo['fields']['status']
+        repository = phab.get_repository(repo)
+        ctx['status'] = repository['fields']['status']
         ctx['urls'] = [
             u['fields']['uri']['display'] for u in
-            repo['attachments']['uris']['uris']
+            repository['attachments']['uris']['uris']
         ]
 
         # Lookup policy details
-        policy = repo['fields']['policy']
+        policy = repository['fields']['policy']
         policies = phab.get_policies(list(set(policy.values())))
         ctx['policy']['view'] = policies[policy['view']]
         ctx['policy']['edit'] = policies[policy['edit']]

striker/templates/tools/repo.html
@@ -10,7 +10,7 @@
   <li><a href="{% url 'tools:index' %}">{% trans "Tools" %}</a></li>
   <li><a href="{% url 'tools:tool' tool=tool.name %}">{{ tool.name }}</a></li>
   <li>{% trans "Repositories" %}</li>
-  <li class="active">{{ repo_name }}</li>
+  <li class="active">{{ repo }}</li>
 </ol>
 {{ block.super }}
 {% endblock %}
@@ -22,7 +22,7 @@
     <div class="panel-body">
       <dl class="dl-horizontal">
         <dt>{% trans "Name" %}</dt>
-        <dd>{{ repo_name }}</dd>
+        <dd>{{ repo }}</dd>
         <dt>{% trans "Status" %}</dt>
         <dd>{{ status }}</dd>
         <dt>{% trans "URLs" %}</dt>

striker/templates/tools/tool.html
@@ -33,7 +33,7 @@
     <div class="panel-body">
       <ul class="tools-diffusion-repos">
         {% for repo in repos %}
-        <li><a href="{% url 'tools:repo_view' tool=tool.name name=repo.name %}">{{ repo.name }}</a></li>
+        <li><a href="{% url 'tools:repo_view' tool=tool.name repo=repo.name %}">{{ repo.name }}</a></li>
         {% endfor %}
       </ul>
     </div>

striker/tools/urls.py
@@ -21,20 +21,23 @@
 from django.conf import urls
 
 
+TOOL = r'(?P<tool>[_a-z][-0-9_a-z]*)'
+REPO = r'(?P<repo>[_a-z][-0-9_a-z]*)'
+
 urlpatterns = [
     urls.url(r'^$', 'striker.tools.views.index', name='index'),
     urls.url(
-        r'^(?P<tool>[_a-z][-0-9_a-z]*)$',
+        r'^id/{tool}$'.format(tool=TOOL),
         'striker.tools.views.tool',
         name='tool'
     ),
     urls.url(
-        r'^(?P<tool>[_a-z][-0-9_a-z]*)/create/repo$',
+        r'^id/{tool}/repos/create$'.format(tool=TOOL),
         'striker.tools.views.repo_create',
         name='repo_create'
     ),
     urls.url(
-        r'^(?P<tool>[_a-z][-0-9_a-z]*)/view/repo/(?P<name>[a-z0-9-]+)$',
+        r'^id/{tool}/repos/id/{repo}$'.format(tool=TOOL, repo=REPO),
         'striker.tools.views.repo_view',
         name='repo_view'
     ),

commit 3ea6ba239fd49a76222f702eb447c02422e68543

repo creation: Fix redirect and add error handling
* Give the user an error notice rather than a fatal error when no
  phabricator accounts can be found for the tool admins.
* Fix post-creation redirect parameters broken by 33520dc

striker/tools/views.py
@@ -119,21 +119,26 @@ def repo_create(req, tool):
             #   longer be able to edit the object. (ERR-CONDUIT-CORE)
             # Convert list of maintainers to list of phab users
             maintainers = [m.full_name for m in tool.maintainers()]
-            phab_maintainers = [m['phid'] for m in phab.user_ldapquery(
-                maintainers)]
-            # Create repo
-            repo = phab.create_repository(name, phab_maintainers)
-            # Save a local association between the repo and the tool.
-            repo_model = DiffusionRepo(
-                tool=tool.name, name=name, phid=repo['phid'],
-                created_by=req.user)
-            repo_model.save()
-            # Redirect to repo view
-            return shortcuts.redirect(
-                urlresolvers.reverse('tools:repo_view', kwargs={
-                    'tool': tool.name,
-                    'name': name,
-                }))
+            try:
+                phab_maintainers = [m['phid'] for m in phab.user_ldapquery(
+                    maintainers)]
+            except KeyError:
+                messages.error(req,
+                    'No Phabricator accounts found for tool maintainers.')
+            else:
+                # Create repo
+                repo = phab.create_repository(name, phab_maintainers)
+                # Save a local association between the repo and the tool.
+                repo_model = DiffusionRepo(
+                    tool=tool.name, name=name, phid=repo['phid'],
+                    created_by=req.user)
+                repo_model.save()
+                # Redirect to repo view
+                return shortcuts.redirect(
+                    urlresolvers.reverse('tools:repo_view', kwargs={
+                        'tool': tool.name,
+                        'repo': name,
+                    }))
 
     return shortcuts.render(req, 'tools/repo/create.html', {
         'tool': tool, 'form': form})