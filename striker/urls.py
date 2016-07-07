4b3511495e536be43ace1e5a4ed09cbeabc7a1d4
Bryan Davis <bd808@wikimedia.org>
2016-07-06 22:58:01 -0600

    striker.urls: remove unused import

--- striker/urls.py
+++ striker/urls.py
@@ -21,7 +21,6 @@
 from django.conf import settings
 from django.conf import urls
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
 import ratelimitbackend.admin
 
 import striker.labsauth.urls
