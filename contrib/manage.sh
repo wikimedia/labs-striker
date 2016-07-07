e97827e580b9b2ce06aeb977fe92c6a192f42d70
Bryan Davis <bd808@wikimedia.org>
2016-07-06 20:20:15 -0600

    contrib: fix manage.sh directory detection

--- contrib/manage.sh
+++ contrib/manage.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Run manage.py using the MediaWiki-Vagrant created virtualenv.
 #
-STRIKER_DIR=$(dirname $(dirname $(readlink -f manage.sh)))
+STRIKER_DIR=$(dirname $(dirname $(readlink -f $0)))
 exec sudo /usr/bin/env ${STRIKER_DIR}/.venv/bin/python ${STRIKER_DIR}/manage.py "$@"
