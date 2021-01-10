d1095f18cc4a48849d4cc51a83fdeac0bdd393c5
Andrew Bogott <abogott@wikimedia.org>
2021-01-10 12:37:06 -0600

    Refactor collectstatic.sh to be run as a post-install step
    This will allow us to generate static content as part of the
    deployment script rather than requiring is as a separate build step.
    
    Bug: T271621
    Change-Id: I9178e366b5462b487d3de7b688d0adecf5146cde

--- contrib/collectstatic.sh
+++ contrib/collectstatic.sh
@@ -2,6 +2,8 @@
 # Ugly collectstatic command to keep django-autocomplete-light from making
 # a mess of the staticfiles directory.
 #
+# This is meant to be run as a post-install script on production targets.
+#
 # Django's collect static maintenance script has an --ignore option, but
 # strangely the way that it is implemented inside the script you can only
 # exclude based on the name of a single directory or file. Ideally you would be
@@ -9,8 +11,11 @@
 # django-autocomplete-light/vendor/vendor
 #
 set -e
+set -x
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-$DIR/manage.sh collectstatic -c --noinput \
+
+STRIKER_DIR=$(dirname $(dirname $(readlink -f $0)))
+exec sudo /usr/bin/env ${VENV}/bin/python ${STRIKER_DIR}/manage.py collectstatic -c --noinput \
     --ignore src \
     --ignore tests \
     --ignore *.json \
