7b3c42da03390a12cbe78a67d324574a3b9f12d6
Taavi Väänänen <hi@taavi.wtf>
2024-03-06 14:06:30 +0200

    Display membership requests in navbar for admins
    Bug: T316832
    Change-Id: I7cf6cb4acf03218d1f213d85f2dae6311c20bee1

---
+++ striker/tools/templatetags/membership.py
@@ -0,0 +1,31 @@
+"""Template tags relating to user Toolforge membership and admin status. """
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2024 Wikimedia Foundation and contributors.
+# All Rights Reserved.
+#
+# This file is part of Striker.
+#
+# Striker is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Striker is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Striker.  If not, see <http://www.gnu.org/licenses/>.
+
+from django import template
+
+from striker.tools.utils import tools_admin
+
+register = template.Library()
+
+
+@register.filter()
+def is_admin(user):
+    return tools_admin(user)
