25566675969dc2bb02abd0f2d7ab5a5637f8539e
Taavi Väänänen <hi@taavi.wtf>
2024-03-07 21:30:00 +0200

    Add test to make sure app boots up properly
    Change-Id: I347f6ff92dab4527d0e8be43814b1b2f86b192f2

---
+++ striker/tests.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2024 Wikimedia Foundation and contributors.
+# All Rights Reserved.
+#
+# This file is part of Striker.
+#
+# Striker is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Striker is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Striker.  If not, see <http://www.gnu.org/licenses/>.
+
+
+def test_e400(client):
+    response = client.get("/e400")
+    assert response.status_code == 400
+    assert b"400 Bad Request" in response.content
